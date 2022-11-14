4f448703881abb2baf83824c8df177547bbeb310
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-11-14 12:47:45 +0100

    fix(api-ub): fix resolver api

--- apps/api-ub/pages/api/v1/resolver/[id].js
+++ apps/api-ub/pages/api/v1/resolver/[id].js
@@ -4,62 +4,47 @@ export default async function handler(req, res) {
     method,
   } = req
 
-
   const query = `
-    PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX  dct:  <http://purl.org/dc/terms/>
 
-    ASK { 
-      GRAPH ?g { 
-        VALUES ?id { "${id}" }
-        ?s  dct:identifier ?id .
-      }
+  ASK { 
+    GRAPH ?g { 
+      VALUES ?id { "${id}" }
+      ?s  dct:identifier ?id .
     }
+  }
 `
 
   switch (method) {
     case 'GET':
       // Find service with ID
       try {
-        const response = await Promise.allSettled([
-          fetch(`${process.env.SKA_API}${query}`).then(res => res.json())
-            .then(res => {
-              if (res && res.boolean) {
-                return {
-                  name: 'skeivtarkiv',
-                  url: process.env.SKA_API,
-                }
+        const response = await Promise.all([
+          fetch(`${process.env.SKA_API}${query}`).then(res => res.json()).then(res => {
+            if (res.boolean) {
+              return {
+                name: 'skeivtarkiv',
+                url: process.env.SKA_API,
               }
-            })
-            .catch(err => { return err }),
-          fetch(`${process.env.MARCUS_API}${query}`).then(res => res.json())
-            .then(res => {
-              if (res && res.boolean) {
-                return {
-                  name: 'marcus',
-                  url: process.env.MARCUS_API,
-                }
+            }
+          }),
+          fetch(`${process.env.MARCUS_API}${query}`).then(res => res.json()).then(res => {
+            if (res.boolean) {
+              return {
+                name: 'marcus',
+                url: process.env.MARCUS_API,
               }
-            })
-            .catch(err => { return err }),
-        ])
-          .then(res => res.filter(Boolean)).then(res => res[0])
-          .catch((err) => {
-            // log that I have an error, return the entire array;
-            console.log('A promise failed to resolve', err);
-            res.status(503).json({ message: err })
+            }
           })
+        ]).then(res => res.filter(Boolean)).then(res => res[0])
 
-
-        if (response.status >= 200 && response.status <= 299) {
+        if (response) {
           res.status(200).json(response)
-        }
-        if (response.status >= 500 && response.status <= 599) {
-          res.status(500).json({ message: "Error" })
         } else {
           res.status(404).json({ message: "ID not found in any services." })
         }
       } catch (err) {
-        err => { console.log(err); return err }
+        err => { return err }
       }
 
       break
