943282a19bfa56329fc432b033c28912742177f9
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-11-17 14:32:31 +0100

    add schema

---
+++ apps/exh-nt/src/lib/munaPlugin/src/schemas/classes/persistent/conceptual/type/Role.js
@@ -0,0 +1,47 @@
+import { FaTag } from 'react-icons/fa'
+import { defineType } from 'sanity'
+import { defaultFieldsets } from '../../../../../fieldsets/defaultFieldsets'
+import { coalesceLabel } from '../../../../../helpers/coalesceLabel'
+import { altLabel, definition, label } from '../../../../properties/datatype'
+
+export default defineType({
+  name: 'Role',
+  title: 'Rolle',
+  type: 'document',
+  initialValue: {
+    editorialState: 'published',
+    accessState: 'open',
+  },
+  icon: FaTag,
+  fieldsets: defaultFieldsets,
+  fields: [
+    label,
+    altLabel,
+    definition,
+    {
+      name: 'activityStream',
+      title: 'Aktivitetsstrøm',
+      titleEN: 'Activity stream',
+      description: 'Events and activities connected to this object',
+      type: 'array',
+      of: [{ type: 'Creation' }],
+      options: {
+        semanticSanity: {
+          '@container': '@list',
+          '@type': '@id'
+        }
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'label',
+    },
+    prepare(selection) {
+      const { title } = selection
+      return {
+        title: coalesceLabel(title),
+      }
+    },
+  },
+})
---
+++ apps/exh-nt/src/lib/munaPlugin/src/schemas/properties/object/timespanSingleton.js
@@ -0,0 +1,12 @@
+export const timespanSingleton = {
+  name: 'timespan',
+  title: 'Tidsspenn',
+  titleEN: 'Timespan',
+  type: 'Timespan',
+  options: {
+    editModal: 'fullscreen',
+    semanticSanity: {
+      '@type': '@id'
+    }
+  },
+};
