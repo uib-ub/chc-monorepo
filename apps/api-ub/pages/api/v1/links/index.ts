d2b8c3b3a65441459bc79070fcf5fd03d3d88e59
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-11-28 16:10:52 +0100

    feat(url-shortener): add qr and nanoid to POST

--- apps/api-ub/pages/api/v1/links/index.ts
+++ apps/api-ub/pages/api/v1/links/index.ts
@@ -1,21 +1,65 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { XataClient } from '../../../../utils/xata'
+import { LinksRecord, XataClient } from '../../../../utils/xata'
+import QRCode from 'qrcode'
+import { nanoid } from 'nanoid'
 
 const xata = new XataClient()
 
+const generateQR = async (text: string) => {
+  try {
+    return await QRCode.toString(text, { type: 'svg' })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 const getLinks = async () => {
   return await xata.db.links.getAll();
+}
+
+const createLink = async (data: LinksRecord) => {
+  return await xata.db.links.create(data);
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
-    const links = await getLinks()
-    res.status(200).json(links)
-  } else {
-    // Handle any other HTTP method
+  try {
+    switch (req.method) {
+      case 'GET': {
+        const links = await getLinks()
+        res.status(200).json(links)
+      }
+      case 'POST': {
+        // Get the path
+        const path = nanoid(8)
+        // Set date
+        const date = new Date()
+        // create QR as SVG
+        const qr = await generateQR(req.body.originalURL)
+
+        const newLink = {
+          ...req.body,
+          path,
+          created: date,
+          modified: date,
+          qr
+        }
+
+        const links = await createLink(newLink)
+        res.status(200).json(links)
+      }
+      default:
+        res.status(405).json({
+          error: { message: 'Method not allowed' },
+        })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      error: { message: `An error ocurred, ${err}` },
+    })
   }
 }
